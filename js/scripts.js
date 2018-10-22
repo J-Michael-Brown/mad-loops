88d4734a688e2dd6cb3e1e08da06890ab1a80233
arashi walker, Michael Brown and Robert Lee <arashi@>
2018-10-22 13:18:44 -0700

    Add working fixes to code

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,17 @@
 $(document).ready(function() {
 
   $("form#form1").submit(function(event) {
-    var input1 = $("input#input1").val();
-    var input2 = $("input#input2").val();
-    var input3 = $("input#input3").val();
-    var input4 = $("input#input4").val();
-    var input5 = $("input#input5").val();
 
-    $(".outputform1").append(input1);
-    $(".outputform2").append(input2);
-    $(".outputform3").append(input3);
-    $(".outputform4").append(input4);
-    $(".outputform5").append(input5);
+    var blanks =["adj1", "animal1", "verb", "adj2", "animal2"];
+
+    blanks.forEach(function(blank) {
+      var userInput = $("input#" + blank).val();
+      $("." + blank).text(userInput);
+
+    });
 
     $("#hidden").show();
 
     event.preventDefault();
   });
-
-
-
 });
